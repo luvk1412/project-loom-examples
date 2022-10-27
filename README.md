fc2a6743296c8455f3675392ce6c1159a376dac3
Luv <luvk1412@gmail.com>
2022-10-27 10:13:47 +0530

    added more cases and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # project-loom-examples
+
+### IDEA Run Instructions
+
+- Got File > Project Structure > Project Settings > Project and choose appropriate JDK 19 and choose language level as 19 preview.
+- Go to Run/Debug Configuration > Modify Options and enable "Add VM options" and enter "--enable-preview" in VM options.
+- Now running the main function should run things.
--- src/main/java/experiment/ManyToOneMappingCheck.java
+++ src/main/java/experiment/ManyToOneMappingCheck.java
@@ -2,25 +2,34 @@ package experiment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ManyToOneMappingCheck {
 
   /**
-   * Helps in checking that same thread after sleep is picked up by another platform thread.
+   * Helps in checking that same virtual thread after sleep is picked up by another platform thread.
    */
   public static void main(String[] args) throws InterruptedException {
+    manyToOneNormalCheck();
+    System.out.println("\n\n\n\n --- Completed --- \n\n\n\n");
+    manyToOneLockCheck();
+    System.out.println("\n\n\n\n --- Completed --- \n\n\n\n");
+    manyToOneSynchronizedCheck();
+  }
+
+  private static void manyToOneNormalCheck() throws InterruptedException {
     List<Thread> threads = new ArrayList<>();
     Thread t;
     for (int i = 0; i < 100; ++i) {
       final int ii = i;
       t = Thread.ofVirtual().factory().newThread(() -> {
-        System.out.println(ii + ": " + Thread.currentThread());
+        printCurrentThread(ii, "NormalCheck");
         try {
           Thread.sleep(10);
         } catch (InterruptedException e) {
           throw new RuntimeException(e);
         }
-        System.out.println(ii + ": " + Thread.currentThread());
+        printCurrentThread(ii, "NormalCheck");
       });
       t.start();
       threads.add(t);
@@ -28,6 +37,64 @@ public class ManyToOneMappingCheck {
     for (Thread thread : threads) {
       thread.join();
     }
+  }
+
+  private static void manyToOneLockCheck() throws InterruptedException {
+    ReentrantLock lock = new ReentrantLock();
+    List<Thread> threads = new ArrayList<>();
+    Thread t;
+    for (int i = 0; i < 100; ++i) {
+      final int ii = i;
+      t = Thread.ofVirtual().factory().newThread(() -> {
+        lock.lock();
+        printCurrentThread(ii, "LockCheck");
+        lock.unlock();
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+        lock.lock();
+        printCurrentThread(ii, "LockCheck");
+        lock.unlock();
+      });
+      t.start();
+      threads.add(t);
+    }
+    for (Thread thread : threads) {
+      thread.join();
+    }
+  }
+
+  private static void manyToOneSynchronizedCheck() throws InterruptedException {
+    List<Thread> threads = new ArrayList<>();
+    Object lock = new Object();
+    Thread t;
+    for (int i = 0; i < 100; ++i) {
+      final int ii = i;
+      t = Thread.ofVirtual().factory().newThread(() -> {
+        synchronized (lock) {
+          printCurrentThread(ii, "SynchronisedCheck");
+        }
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+        synchronized (lock) {
+          printCurrentThread(ii, "SynchronisedCheck");
+        }
+      });
+      t.start();
+      threads.add(t);
+    }
+    for (Thread thread : threads) {
+      thread.join();
+    }
+  }
+
+  private static void printCurrentThread(final int count, String suffix) {
+    System.out.println(suffix + ":" + count + ": " + Thread.currentThread());
   }
 
 }
